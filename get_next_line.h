466509de1845ac3ce535c0c5acb711fb9ae9d635
awerebea <awerebea.21@gmail.com>
2020-05-13 13:01:13 +0400

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awerebea <awerebea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/13 12:42:55 by awerebea          #+#    #+#             */
+/*   Updated: 2020/05/13 12:56:57 by awerebea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
